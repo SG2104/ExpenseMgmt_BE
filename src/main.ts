2ff7d85eef07cf315d40d1231d5e2fed799fb096
SG2104 <shreyalganna21@gmail.com>
2025-03-02 14:42:30 -0500

    feat: setup config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  Logger.log(`***** Listening on port:${process.env.PORT} *****`);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
